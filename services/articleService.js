1b5688f2074fd8ad6d3250453b72889b2bab2d8a
ShineLee <shine@vip.qq.com>
2017-08-11 23:29:50 +0800

    增加请求http头

--- services/articleService.js
+++ services/articleService.js
@@ -58,11 +58,10 @@ service.add = function (req, res, next) {
                     readability(url, {
                         headers: {
                             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                            'Accept-Encoding': 'gzip, deflate',
+                            'Accept-Encoding': 'deflate',
                             'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh-TW;q=0.4',
                             'Cache-Control': 'max-age=0',
                             'Connection': 'keep-alive',
-                            'Referer': urlObj.hostname,
                             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
                         }
                     }, function (err, article) {
