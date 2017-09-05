0dab6454ecef60a3645f159900a3e77b6786371f
ShineLee <shine@vip.qq.com>
2017-09-05 16:04:25 +0800

    尝试百度语音api

--- libs/baseService.js
+++ libs/baseService.js
@@ -1,61 +1,63 @@
-'use strict';
-var mongodb = require('./mongodb.js');
-var redis = require('./redis.js').redisClient;
-var utils = require('./utils.js');
-var loader = require('./loadConfig');
-var RESULT_TRUE = 'TRUE';
-var RESULT_FALSE = 'FALSE';
-var need_encrypt = require(loader.configFile)[loader.projectName].need_encrypt;
 
-class BaseService{
+const mongodb = require('./mongodb.js');
+const redis = require('./redis.js').redisClient;
+const utils = require('./utils.js');
+const loader = require('./loadConfig');
+
+const RESULT_TRUE = 'TRUE';
+const RESULT_FALSE = 'FALSE';
+const config = require(loader.configFile)[loader.projectName];
+
+class BaseService {
     constructor() {
         this.redis = redis;
+        this.config = config;
         this.restSuccess = restSuccess;
-        this.restError  = restError;
-    };
+        this.restError = restError;
+    }
 }
 
 /**
  * 处理成功返回
  */
-function restSuccess(res, data, other_datas){
-    var result = {result: RESULT_TRUE};
+function restSuccess(res, data, other_datas) {
+    let result = { result: RESULT_TRUE };
     if (data) {
         result.data = data;
     }
     if (other_datas) {
-        for (var key in other_datas) {
+        for (const key in other_datas) {
             result[key] = other_datas[key];
         }
     }
-    if(need_encrypt){
+    if (config.need_encrypt) {
         result = utils.encodeResult(JSON.stringify(result));
-        rest(res,{encode_str:result});
-    }else{
-        rest(res,result);
+        rest(res, { encode_str: result });
+    } else {
+        rest(res, result);
     }
 }
 
 /**
  * 处理错误返回
  */
-function restError(res,err_code, err_msg){
-    var result = {result: RESULT_FALSE};
+function restError(res, err_code, err_msg) {
+    let result = { result: RESULT_FALSE };
     if (err_code) {
         result.errorcode = err_code;
     }
     if (err_msg) {
         result.msg = err_msg;
     }
-    if(need_encrypt){
+    if (config.need_encrypt) {
         result = utils.encodeResult(JSON.stringify(result));
-        rest(res,{encode_str:result});
-    }else{
-        rest(res,result);
+        rest(res, { encode_str: result });
+    } else {
+        rest(res, result);
     }
 }
 
-function rest(res, data){
+function rest(res, data) {
     res.send(data);
 }
 
