bb16e4cfa98347bb648e43113e6a5eae6bdcd0ad
shine <shine@goldmorning.cn>
2020-01-03 17:02:18 +0800

    update dependencies

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,11 +1,38 @@
-var mongoose = require('mongoose');
-var Promise = require('bluebird');
-Promise.promisifyAll(mongoose);
-var loader = require('./loadConfig.js');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
 
-var mongoConfig = require(loader.configFile)[loader.projectName].mongodb;
-mongoose.connect('mongodb://' + mongoConfig.user + ':' + mongoConfig.pwd + '@' + mongoConfig.host + ':' + mongoConfig.port + '/' + mongoConfig.db);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+mongoose.Promise = Promise;
+
+Promise.promisifyAll(mongoose);
+const loader = require('./loadConfig');
+
+const mongoConfig = require(loader.configFile)[loader.projectName].mongodb; // eslint-disable-line
+
+let connectStr = 'mongodb://';
+if (mongoConfig.replication) {
+    if (mongoConfig.user) {
+        connectStr += `${mongoConfig.user}:${mongoConfig.pwd}@`;
+    }
+    mongoConfig.servers.forEach((server) => {
+        connectStr += `${server.host}:${server.port},`;
+    });
+    connectStr = connectStr.substring(0, connectStr.length - 1);
+    connectStr += `/${mongoConfig.db}?replicaSet=${mongoConfig.replication}`;
+} else if (mongoConfig.user) {
+    connectStr += `${mongoConfig.user}:${mongoConfig.pwd}@${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.db}`;
+} else {
+    connectStr += `${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.db}`;
+}
+// connectStr += `?authSource=${mongoConfig.db}`;
+console.log(connectStr);
+mongoose.connect(connectStr, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    socketTimeoutMS: 0,
+    keepAlive: true,
+    reconnectTries: 30
+}).catch((e) => {
+    console.error(e);
+});
 
 module.exports = mongoose;
